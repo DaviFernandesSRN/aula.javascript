f4b906e74525dc67f1b8d22c4a5ab965ddd54c6d
Davi Da Silva Fernandes.T_DISYS <davi.fernandes.T_DISYS@grupoboticario.com.br>
2023-05-24 22:50:17 -0300

    add new pages

---
+++ js/condicao.js
@@ -0,0 +1,38 @@
+//--usando if e else
+//var idade = prompt("qual sua idade");
+
+//if(idade >= 18){
+//    alert("maior de idade");
+//}else{
+//    alert("menor de idade");
+//};
+
+//ESTRUTURA DE REPETIÇÃO COM WHILE
+// var count =0;
+// while(count <= 5){
+// console.log(count);
+// count = count + 1;
+// };
+
+//ESTRUTURA DE REPETIÇÃO COM WHILE ++ e alert
+
+// var count =0;
+//  while(count <= 5){
+// alert(count);
+//  count ++;
+//  };
+
+//usando o for
+// var count;
+// for(count=1; count <= 5; count ++){
+//     alert(count);
+// }
+
+// var d = new Date();
+// alert(d);
+
+//var d = new Date();
+//alert(d.getMonth()+1);
+
+// var d = new Date();
+// alert(d.getDay());
--- js/meuprimeirojs.js
+++ js/meuprimeirojs.js
@@ -1,4 +1,12 @@
 
 var nome = "Davi Fernandes";
 var idade = 30;
-alert(nome + "tem " + idade + " anos" );
+var idade2 = 9;
+var frase ="vasco é o melhor time do mundo";
+//alert(nome + "tem " + idade + " anos" );
+//alert( idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+//console.log(frase.replace("vasco", "flamengo" ));
+alert(frase.toUpperCase());
+
